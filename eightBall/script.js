171085fa32882cc6cb68d8c14d6e57e1759adc03
Daemon220 <hartzerkaile@icloud.com>
2024-03-04 13:22:40 +0200

    Magic Eightball

---
+++ eightBall/script.js
@@ -0,0 +1,24 @@
+function shake() {
+    let ball = document.getElementById("ball");
+    let messageText = document.getElementById("message");
+  
+    if (messageText != null) {
+      messageText.parentNode.removeChild(messageText);
+    }
+  
+    ball.classList.add("shake");
+  
+    setTimeout(function(){ball.classList.remove("shake");}, 1000);
+    setTimeout(function(){getFortune();}, 1000);
+  
+    let fortunes = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 
+                    'Cannot predict now', 'Do not count on it', 'My sources say no', 
+                    'Outlook not so good', 'Signs point to yes'];
+    let fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  
+    let parent = document.getElementById("fortune");
+    let newMessage = document.createElement("div");
+    newMessage.setAttribute('id', "message");
+    newMessage.innerHTML = "\"" + fortune + "\"";
+    parent.appendChild(newMessage);
+  }
