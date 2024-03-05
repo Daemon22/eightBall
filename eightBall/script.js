7d4afe363411bd5104bdbdcf047324f2898c2bba
Daemon220 <hartzerkaile@icloud.com>
2024-03-05 09:55:04 +0200

    css

--- eightBall/script.js
+++ eightBall/script.js
@@ -1,6 +1,7 @@
 function shake() {
     let ball = document.getElementById("ball");
     let messageText = document.getElementById("message");
+    let questionInput = document.getElementById("question");
   
     if (messageText != null) {
       messageText.parentNode.removeChild(messageText);
@@ -15,10 +16,13 @@ function shake() {
                     'Cannot predict now', 'Do not count on it', 'My sources say no', 
                     'Outlook not so good', 'Signs point to yes'];
     let fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  
+    
+    let question = questionInput.value;
     let parent = document.getElementById("fortune");
     let newMessage = document.createElement("div");
     newMessage.setAttribute('id', "message");
-    newMessage.innerHTML = "\"" + fortune + "\"";
+    newMessage.innerHTML = `"${question}" - "${fortune}"`;
     parent.appendChild(newMessage);
+
+    questionInput.value = "";
   }
